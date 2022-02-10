8f55da9db248d12ed6ae0f3760a0e8b8306d875a
Abdulsalam-Aljamal <abdulsalam.aljamal@gmail.com>
2022-02-10 22:31:41 +0200

    Adding the Welcome and Login destinations and navigate between them

---
+++ starter/app/src/main/java/com/udacity/shoestore/screens/login/LoginFragment.kt
@@ -0,0 +1,42 @@
+package com.udacity.shoestore.screens.login
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.findNavController
+import com.udacity.shoestore.R
+import com.udacity.shoestore.databinding.FragmentLoginBinding
+
+class LoginFragment : Fragment() {
+
+    private lateinit var binding : FragmentLoginBinding
+
+    private lateinit var loginButton : Button
+    private lateinit var createAccountButton : Button
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment using binding
+        binding = DataBindingUtil.inflate(inflater,
+            R.layout.fragment_login, container, false)
+
+        loginButton = binding.loginButton
+        createAccountButton = binding.createAccountButton
+
+        loginButton.setOnClickListener { navigateToWelcome() }
+        createAccountButton.setOnClickListener { navigateToWelcome() }
+
+        return binding.root
+    }
+
+    private fun navigateToWelcome(){
+        val action = LoginFragmentDirections.actionLoginFragmentToWelcomeFragment()
+        findNavController().navigate(action)
+    }
+}
