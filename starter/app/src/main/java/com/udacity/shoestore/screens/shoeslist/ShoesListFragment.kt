491cb8c3d8ea5614aeb16a3280a9ba3aafcbd062
Abdulsalam-Aljamal <abdulsalam.aljamal@gmail.com>
2022-02-12 17:32:39 +0200

    Add the logout menu and make it works

--- starter/app/src/main/java/com/udacity/shoestore/screens/shoeslist/ShoesListFragment.kt
+++ starter/app/src/main/java/com/udacity/shoestore/screens/shoeslist/ShoesListFragment.kt
@@ -1,12 +1,11 @@
 package com.udacity.shoestore.screens.shoeslist
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.databinding.DataBindingComponent
+import android.view.*
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.udacity.shoestore.R
 import com.udacity.shoestore.databinding.FragmentShoesListBinding
 
@@ -20,7 +19,21 @@ class ShoesListFragment : Fragment() {
     ): View {
         // Inflate the layout for this fragment using binding
         binding = DataBindingUtil.inflate(layoutInflater, R.layout.fragment_shoes_list, container, false)
+
+        // Let the fragment know about the menu
+        setHasOptionsMenu(true)
+
+        // Return the outermost view in the layout
         return binding.root
     }
 
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.app_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.onNavDestinationSelected(item, requireView().findNavController())
+                || super.onOptionsItemSelected(item)
+    }
 }
